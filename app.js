e4f6e148a051edeb13357a1c6356a42e65ca1695
ashkam58 <ashkam58@gmail.com>
2023-11-11 13:29:46 +0530

    commit

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ for(let i = 0; i<sectBtn.length; i++)
     sectBtn[i].addEventListener('click', function(){
         let currentBtn = document.querySelectorAll('.active-btn');
         currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-        this.className += 'active-btn';
+        this.className += ' active-btn';
     })
 }
 
 // Section active class
 allSections.addEventListener('click',(e)=>{
+    console.log(e.target);
     const id = e.target.dataset.id;
     if(id){
         //remove selected from the other button
